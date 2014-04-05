be69b2984df86d84ef62d624351f289ef8069f43
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-04-05 16:45:37 -0400

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pybooks',
     version='0.1',
-    scripts=['scripts/pybooks.py']
+    scripts=['scripts/pybooks.py'],
     zip_safe=False,
     install_requires=['requests', 'pyyaml']
 )
