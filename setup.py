03d53021d22c93bde05f76bbf2330703b71a3754
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-04-05 16:44:23 -0400

    Make script installable and add a .gitignore

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='pybooks',
+    version='0.1',
+    scripts=['scripts/pybooks.py']
+    zip_safe=False,
+    install_requires=['requests', 'pyyaml']
+)
+
